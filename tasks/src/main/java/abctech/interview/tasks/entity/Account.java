7ad58d2a7a1c547f591c5fa03284ade6d1c49319
noobnubnoob <minvinkovic@gmail.com>
2025-05-02 18:12:32 +0200

    Finishing Account entity constructors + annotations

--- tasks/src/main/java/abctech/interview/tasks/entity/Account.java
+++ tasks/src/main/java/abctech/interview/tasks/entity/Account.java
@@ -1,20 +1,41 @@
 package abctech.interview.tasks.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.sql.Date;
 
 @Entity
 @Table(name="employee")
+@Data
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="racun_id")
     Integer racunId;
+    @Column(name="iban")
     String iban;
+    @Column(name="tip_racuna")
     Integer tipRacuna;
+    @Column(name="valuta")
     String valuta;
+    @Column(name="klijent_id")
     Integer klijentId;
+    @Column(name="datum_otvaranja")
     Date datumOtvaranja;
+    @Column(name="datum_zatvaranja")
     Date datumZatvaranja;
+
+    // define constructors
+    public Account() {
+
+    }
+
+    public Account(String iban, Integer tipRacuna, String valuta, Integer klijentId) {
+        this.iban = iban;
+        this.tipRacuna = tipRacuna;
+        this.valuta = valuta;
+        this.klijentId = klijentId;
+
+    }
 }
