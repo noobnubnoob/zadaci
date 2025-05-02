ca9fc522838578bfb9ac486b9de872a13326a832
noobnubnoob <minvinkovic@gmail.com>
2025-05-02 20:34:08 +0200

    Service implementation

--- tasks/src/main/java/abctech/interview/tasks/service/AccountServiceImpl.java
+++ tasks/src/main/java/abctech/interview/tasks/service/AccountServiceImpl.java
@@ -1,29 +1,50 @@
 package abctech.interview.tasks.service;
 
+import abctech.interview.tasks.dao.AccountRepository;
 import abctech.interview.tasks.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccountServiceImpl implements AccountService{
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountServiceImpl(AccountRepository theAccountRepository) {
+        accountRepository = theAccountRepository;
+    }
     @Override
     public List<Account> findAll() {
-        return null;
+        return accountRepository.findAll();
     }
 
     @Override
     public Account findById(int theId) {
-        return null;
+        Optional<Account> result =  accountRepository.findById(theId);
+        Account theAccount = null;
+
+        if (result.isPresent()) {
+            theAccount = result.get();
+        }
+        else {
+            // we didn't find the employee
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+
+        return theAccount;
     }
 
     @Override
-    public void save(Account theEmployee) {
-
+    public void save(Account theAccount) {
+        accountRepository.save(theAccount);
     }
 
     @Override
     public void deleteById(int theId) {
-
+        accountRepository.deleteById(theId);
     }
 }
