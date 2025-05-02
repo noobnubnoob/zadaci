acfe13244ccc5cfa6c3fa4a5886e80f10056b6ac
noobnubnoob <minvinkovic@gmail.com>
2025-05-02 13:35:42 +0200

    comments for ArrayOperations class tests for methods in ArrayOperations init

--- tasks/src/main/java/abctech/interview/tasks/arrayutil/ArrayOperations.java
+++ tasks/src/main/java/abctech/interview/tasks/arrayutil/ArrayOperations.java
@@ -4,22 +4,47 @@ import java.util.ArrayList;
 
 public class ArrayOperations {
 
+    /**
+     *
+     * @param array
+     * @return sum of all elements in array
+     */
     public int arraySum(ArrayList<Double> array) {
         return 0;
     }
 
+    /**
+     * Sorts array passed as variable.
+     * @param array
+     * @return sorted array
+     */
     public ArrayList<Integer> sortArray(ArrayList<Integer> array) {
         return array;
     }
 
+    /**
+     * Finds and returns array minimum.
+     * @param array
+     * @return
+     */
     public int arrayMin(ArrayList<Integer> array) {
         return 0;
     }
 
+    /**
+     * Finds and returns array maximum.
+     * @param array
+     * @return
+     */
     public int arrayMax(ArrayList<Integer> array) {
         return 0;
     }
 
+    /**
+     * Calculates and returns arithmetic average of array
+     * @param array
+     * @return array average
+     */
     public double arrayAverage(ArrayList<Integer> array) {
         return 0;
     }
--- tasks/src/test/java/abctech/interview/tasks/arrayutil/ArrayOperationsTests.java
+++ tasks/src/test/java/abctech/interview/tasks/arrayutil/ArrayOperationsTests.java
@@ -1,4 +1,44 @@
 package abctech.interview.tasks.arrayutil;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ArrayOperationsTests {
+    ArrayOperations arrayOperations;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        arrayOperations = new ArrayOperations();
+    }
+
+    @Test
+    @DisplayName("Sum of all array elements")
+    void testArraySum() {
+
+    }
+
+    @Test
+    @DisplayName("Array sort")
+    void testSortArray() {
+
+    }
+
+    @Test
+    @DisplayName("Minimum value in array")
+    void arrayMin() {
+
+    }
+
+    @Test
+    @DisplayName("Maximum value in array")
+    void arrayMax() {
+
+    }
+
+    @Test
+    @DisplayName("Arithmetic value of array")
+    void testArrayAverage() {
+
+    }
 }
